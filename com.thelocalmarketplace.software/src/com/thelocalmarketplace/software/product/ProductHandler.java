eeed34fff394c74d72783200228c6ae224426f61
tararad <tararad@localhost>
2024-03-28 21:35:36 -0600

    implemented handle bulky item

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/product/ProductHandler.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/product/ProductHandler.java
@@ -5,6 +5,7 @@ import com.jjjwelectronics.scale.IElectronicScale;
 import com.jjjwelectronics.scanner.IBarcodeScanner;
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 import com.thelocalmarketplace.software.SelfCheckoutStationSoftware;
+import com.thelocalmarketplace.software.oldCode.Order;
 
 public class ProductHandler {
 	// Things to listen to (hardware)
@@ -48,12 +49,20 @@ public class ProductHandler {
 		handheldScanner.register(scannerListener);
 	}
 	
+	public void handleBulkyItem(double productWeight) {
+		software.setStationBlock(true);
+		System.out.println("No-bagging request is in progress.");
+		
+		System.out.println("Request has been approved");
+		double currentWeight = software.getTotalOrderWeightInGrams();
+		double finalWeight = currentWeight-productWeight;
+		if (finalWeight >= 0) software.addTotalOrderWeightInGrams(-productWeight); 
+	}
 	// all product logic goes here
 	
 	// In order to access the hardware of the SelfCheckoutStation, use software.HARDWARE_YOU_WANNA_GET()
 	
 	// bulky item code also goes in this class i can do it later. Button needed in GUI -Tara
-	// request attendants attention also goes here. Button needed in GUI
-	// safe to say most things go here
-	
+	// request attendants attention also goes in communication, but where? idk. Button needed in GUI
+	// safe to say most things go here for group 4 :))
 }
