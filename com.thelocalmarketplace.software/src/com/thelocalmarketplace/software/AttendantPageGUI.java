df4c43c2fc540b9799ba60ff9d53019fd9c15dc0
Binish Khalid <bkhalid623@gmail.com>
2024-03-30 19:02:39 -0600

    fix mistakes

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AttendantPageGUI.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AttendantPageGUI.java
@@ -5,15 +5,19 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class AttendantPageGUI extends JFrame{
+
 	 public AttendantPageGUI() {
+		 
+		  //setup
 	        setTitle("Attendant Page");
 	        setSize(800, 600);
 	        setDefaultCloseOperation(EXIT_ON_CLOSE);
 	        setLocationRelativeTo(null);
-
+	        
+	        //main panel
 	        JPanel mainPanel = new JPanel(new GridLayout(5, 1));
 
-	        
+	        //label for stations
 	        JLabel stationsLabel = new JLabel("Checkout Stations: ");
 	        // Button panel for managing customer checkout stations
 	        JPanel stationPanel = new JPanel(new GridLayout(2, 2));
@@ -21,21 +25,20 @@ public class AttendantPageGUI extends JFrame{
 	        JButton checkout2Button = new JButton("Checkout Station 2");
 	        JButton checkout3Button = new JButton("Checkout Station 3");
 	        JButton checkout4Button = new JButton("Checkout Station 4");
-	        
-	      
-	        
+	       
 	        stationPanel.add(checkout1Button);
 	        stationPanel.add(checkout2Button);
 	        stationPanel.add(checkout3Button);
 	        stationPanel.add(checkout4Button);
 
-	        
+	        //label for station controls
 	        JLabel stationControlLabel = new JLabel("Checkout Station Controls: ");
+	        // Button panel for station controls 
 	        JPanel stationControlPanel = new JPanel(new FlowLayout());
 	        JButton startStation = new JButton("Start Station");
 	        JButton blockStation = new JButton("Block Station");
-	        JButton unblockStation = new JButton("Unlock Station");
-	        JButton closeStation = new JButton("close Station");
+	        JButton unblockStation = new JButton("Unblock Station");
+	        JButton closeStation = new JButton("Close Station");
 	        
 	        stationControlPanel.add(startStation);
 	        stationControlPanel.add(blockStation);
@@ -43,7 +46,7 @@ public class AttendantPageGUI extends JFrame{
 	        stationControlPanel.add(closeStation);
 	        
 	        
-	        // Adding the station panel to the main panel
+	        // Adding the panels to the main panel
 	        mainPanel.add(stationsLabel);
 	        mainPanel.add(stationPanel);
 	        mainPanel.add(stationControlLabel);
