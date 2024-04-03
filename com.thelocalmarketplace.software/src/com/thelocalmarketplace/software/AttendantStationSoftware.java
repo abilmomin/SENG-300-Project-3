14462a7883cb4bcc215e5a66cee082ccf999b75f
Duncan McKay <duncan.mckay@ucalgary.ca>
2024-04-03 17:35:12 -0600

    add maintenance for printer and coin/banknote amounts.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AttendantStationSoftware.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AttendantStationSoftware.java
@@ -28,9 +28,28 @@ Nami Marwah              30178528
 
 package com.thelocalmarketplace.software;
 
-import javax.swing.*; 
+import javax.swing.*;
+
+import com.jjjwelectronics.OverloadedDevice;
+import com.jjjwelectronics.printer.IReceiptPrinter;
+import com.jjjwelectronics.printer.ReceiptPrinterBronze;
+import com.tdc.CashOverloadException;
+import com.tdc.banknote.Banknote;
+import com.tdc.banknote.BanknoteStorageUnit;
+import com.tdc.banknote.IBanknoteDispenser;
+import com.tdc.coin.Coin;
+import com.tdc.coin.CoinStorageUnit;
+import com.tdc.coin.ICoinDispenser;
+import com.thelocalmarketplace.hardware.ISelfCheckoutStation;
+
+import ca.ucalgary.seng300.simulation.SimulationException;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+import java.util.Map;
 
 public class AttendantStationSoftware extends JFrame{
 //	private AttendantStation attendantStation;
@@ -49,6 +68,10 @@ public class AttendantStationSoftware extends JFrame{
 	// approve no bagging item request
 	// approve a weight disc 
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L; 
 	private JTextField idField;
     private JPasswordField passwordField;
 
@@ -116,6 +139,54 @@ public class AttendantStationSoftware extends JFrame{
 	    });
 	    // Close the login frame
 	    dispose();
+	}
+	
+	public void emptyCoinStorage(SelfCheckoutStationSoftware cSoftware) {
+		ISelfCheckoutStation cS = cSoftware.getStationHardware();
+		CoinStorageUnit storage = cS.getCoinStorage();
+		storage.unload();
+	}
+	
+	public void emptyBanknoteStorage(SelfCheckoutStationSoftware cSoftware) {
+		ISelfCheckoutStation cS = cSoftware.getStationHardware();
+		BanknoteStorageUnit storage = cS.getBanknoteStorage();
+		storage.unload();
+	}
+	
+	public void refillCoinDispensers(SelfCheckoutStationSoftware cSoftware) throws SimulationException, CashOverloadException {
+		ISelfCheckoutStation cS = cSoftware.getStationHardware();
+		Currency currency = Currency.getInstance("CAD");
+		List<BigDecimal> denominations = cS.getCoinDenominations();
+		Map<BigDecimal, ICoinDispenser> dispensers = cS.getCoinDispensers();
+		for (BigDecimal denomination : denominations) {
+			ICoinDispenser dispenser = dispensers.get(denomination);
+			while (dispenser.hasSpace())
+				dispenser.load(new Coin(currency, denomination));
+		}
+	}
+	
+	public void refillBanknoteDispensers(SelfCheckoutStationSoftware cSoftware) throws SimulationException, CashOverloadException {
+		ISelfCheckoutStation cS = cSoftware.getStationHardware();
+		Currency currency = Currency.getInstance("CAD");
+		BigDecimal[] denominations = cS.getBanknoteDenominations();
+		Map<BigDecimal, IBanknoteDispenser> dispensers = cS.getBanknoteDispensers();
+		for (BigDecimal denomination : denominations) {
+			IBanknoteDispenser dispenser = dispensers.get(denomination);
+			while (dispenser.size() <= dispenser.getCapacity())
+				dispenser.load(new Banknote(currency, denomination));
+		}
+	}
+	
+	public void refillPrinterInk(SelfCheckoutStationSoftware cSoftware) throws OverloadedDevice {
+		ISelfCheckoutStation cS = cSoftware.getStationHardware();
+		IReceiptPrinter printer = cS.getPrinter();
+		printer.addInk(ReceiptPrinterBronze.MAXIMUM_INK - printer.inkRemaining());
+	}
+	
+	public void refillPrinterPaper(SelfCheckoutStationSoftware cSoftware) throws OverloadedDevice {
+		ISelfCheckoutStation cS = cSoftware.getStationHardware();
+		IReceiptPrinter printer = cS.getPrinter();
+		printer.addPaper(ReceiptPrinterBronze.MAXIMUM_PAPER - printer.paperRemaining());
 	}
 
     public static void main(final String[] args) {
