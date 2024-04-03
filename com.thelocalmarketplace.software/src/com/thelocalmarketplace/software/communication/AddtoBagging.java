06f61ba3ea2f77be8e219847ce16dcb6301fe3c2
emily-gv <emilygarciavolk@gmail.com>
2024-04-02 20:52:14 -0600

    Removed unused function

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/communication/AddtoBagging.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/communication/AddtoBagging.java
@@ -41,11 +41,11 @@ public class AddtoBagging extends JFrame {
 	    button.setFont(new Font("Arial", Font.PLAIN, 16));
 	    button.setPreferredSize(new Dimension(100, 30));
 
- 
+	    /*
 	    button.addActionListener(e -> {
 	    	dispose();
-	    	
 	    });
+	    */
 	   
 	    mainPanel.add(Box.createVerticalGlue());
 	    mainPanel.add(bigTextLabel);
