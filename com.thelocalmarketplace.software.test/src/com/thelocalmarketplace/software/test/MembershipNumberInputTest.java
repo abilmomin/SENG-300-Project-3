76b5dd33ac2da0bec60f9aad0bb6f9d5285477c8
tararad <tararad@localhost>
2024-04-04 14:48:00 -0600

    Membership Test Update

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/MembershipNumberInputTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/MembershipNumberInputTest.java
@@ -1,11 +1,11 @@
 package com.thelocalmarketplace.software.test;
 
-import javax.swing.JFrame;
 
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import com.jjjwelectronics.card.Card;
 import com.thelocalmarketplace.software.MembershipCard;
 
 public class MembershipNumberInputTest {
@@ -17,6 +17,25 @@ public class MembershipNumberInputTest {
 	}
 	
 	@Test
+    public void createsSuccessfulMembershipCard() {
+        String cardType = "Membership";
+        String number = "1234567890";
+        String cardholder = "Samantha";
+        String cvv = "101";
+        String pin = "1234";
+        boolean isTapEnabled = true;
+        boolean hasChip = true;
+
+        membershipCard.createMembershipCard(cardType, number, cardholder, cvv, pin, isTapEnabled, hasChip);
+        Card card = membershipCard.getMemberShipCard();
+
+        assertNotNull(card);
+        assertEquals(cardType, card.kind);
+        assertEquals(number, card.number);
+        assertEquals(cardholder, card.cardholder);
+    }
+	
+	@Test
 	public void isMembershipValid() {
 		assertTrue(membershipCard.isMembershipNumberValid("1234567890"));
 	}
@@ -26,5 +45,27 @@ public class MembershipNumberInputTest {
 		assertFalse(membershipCard.isMembershipNumberValid("12345"));
 		assertFalse(membershipCard.isMembershipNumberValid("abc"));
 	}
+	
+	@Test
+    public void assignsValidMembershipNumber() {
+        String memberName = "Samantha";
+        String membershipNumber = membershipCard.createNewMember(memberName);
+
+        assertEquals(memberName, membershipCard.getMemberName());
+        assertEquals("Membership number should be 10 digits long", 10, membershipNumber.length());
+        assertTrue("Membership number should contain only digits", membershipNumber.matches("\\d{10}"));
+        assertNotNull(membershipNumber);
+    }
+	
+	@Test
+	public void returnsCorrectMembershipNumber() {
+		String expectedMembershipNumber = membershipCard.createNewMember("Test");
+		String membershipNumber = membershipCard.getMembershipNumber();
+		assertNotNull(membershipNumber);
+		assertEquals(membershipNumber, expectedMembershipNumber);
+		
+	}
+	
+	
 }
 
