34f449811c9d147921032257593cb2b188c126a3
Talaal Irtija <irtijat@gmail.com>
2024-04-06 17:03:28 -0600

    Added action listeners to card payment for hardware interaction.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/communication/GUI/CustomerStationSoftware/CardPayment.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/communication/GUI/CustomerStationSoftware/CardPayment.java
@@ -1,11 +1,15 @@
 package com.thelocalmarketplace.software.communication.GUI.CustomerStationSoftware;
 
+import com.thelocalmarketplace.software.SelfCheckoutStationSoftware;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class CardPayment extends JFrame {
 
-    public CardPayment() {
+    private int paymentType = 0;
+
+    public CardPayment(SelfCheckoutStationSoftware software, String typeOfCard) {
         setTitle("Select Card Payment Method");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setSize(300, 200);
@@ -13,8 +17,17 @@ public class CardPayment extends JFrame {
         setLayout(new FlowLayout());
 
         JRadioButton swipeButton = new JRadioButton("Swipe");
+        swipeButton.addActionListener(e -> {
+            paymentType = 1;
+        });
         JRadioButton tapButton = new JRadioButton("Tap");
+        tapButton.addActionListener(e -> {
+            paymentType = 2;
+        });
         JRadioButton insertButton = new JRadioButton("Insert Card");
+        insertButton.addActionListener(e -> {
+            paymentType = 3;
+        });
 
         ButtonGroup group = new ButtonGroup();
         group.add(swipeButton);
@@ -28,6 +41,14 @@ public class CardPayment extends JFrame {
 
         JButton finishPaymentButton = new JButton("Finish Payment");
         finishPaymentButton.addActionListener(e -> {
+
+            switch (paymentType) {
+                case 1 -> software.getStationHardware().getCardReader().swipe();
+                case 2 -> software.getStationHardware().getCardReader().tap();
+                case 3 -> software.getStationHardware().getCardReader().insert();
+                default ->
+                        JOptionPane.showMessageDialog(this, "Please select a payment method.", "Payment", JOptionPane.INFORMATION_MESSAGE);
+            }
             // Placeholder action listen
             JOptionPane.showMessageDialog(this, "Payment Method Selected. Processing Payment...", "Payment", JOptionPane.INFORMATION_MESSAGE);
             dispose();
