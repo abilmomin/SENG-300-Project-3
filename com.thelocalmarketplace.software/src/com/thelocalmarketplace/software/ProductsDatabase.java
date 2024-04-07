b43048d2ec463f5c8ef183dc9cd5102e78776b01
Izabella <Izabella.mawani@gmail.com>
2024-04-07 17:54:37 -0600

    javadoc and cleanup for ProductsDatabase

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/ProductsDatabase.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/ProductsDatabase.java
@@ -1,50 +1,66 @@
+/**
+
+Name                      UCID
+
+Yotam Rojnov             30173949
+Duncan McKay             30177857
+Mahfuz Alam              30142265
+Luis Trigueros Granillo  30167989
+Lilia Skumatova          30187339
+Abdelrahman Abbas        30110374
+Talaal Irtija            30169780
+Alejandro Cardona        30178941
+Alexandre Duteau         30192082
+Grace Johnson            30149693
+Abil Momin               30154771
+Tara Ghasemi M. Rad      30171212
+Izabella Mawani          30179738
+Binish Khalid            30061367
+Fatima Khalid            30140757
+Lucas Kasdorf            30173922
+Emily Garcia-Volk        30140791
+Yuinikoru Futamata       30173228
+Joseph Tandyo            30182561
+Syed Haider              30143096
+Nami Marwah              30178528
+
+ */
 package com.thelocalmarketplace.software;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Currency;
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
-import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.Numeral;
 import com.jjjwelectronics.scanner.Barcode;
-import com.jjjwelectronics.scanner.BarcodedItem;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
-import com.thelocalmarketplace.hardware.PLUCodedItem;
 import com.thelocalmarketplace.hardware.PLUCodedProduct;
 import com.thelocalmarketplace.hardware.PriceLookUpCode;
 import com.thelocalmarketplace.hardware.Product;
-import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
 import com.thelocalmarketplace.hardware.external.ProductDatabases;
-import com.thelocalmarketplace.software.SelfCheckoutStationSoftware;
 
 public class ProductsDatabase {
 
 	private static Map<Barcode, BarcodedProduct> productDatabase = ProductDatabases.BARCODED_PRODUCT_DATABASE;
-	private Map<Product, Integer> inventoryDatabase = ProductDatabases.INVENTORY; 
 	private static Map<PriceLookUpCode, PLUCodedProduct> PLUDatabase = ProductDatabases.PLU_PRODUCT_DATABASE; 
 	private static ArrayList<Barcode> barcodes = new ArrayList<>();
 
-	// private ArrayList<BarcodedItems> items 
+	/**
+	 * Constructor for ProductsDatabase class
+	 */
 	public ProductsDatabase() {
 		InitializeBarcodedProducts();
 		InitializePLUProducts();
 	}
 
+	/**
+	 * Initialize bar-codes and assign them to products 
+	 * Add bar-coded products to the database  
+	 */
 	private static void InitializeBarcodedProducts() {
-		// Do we need to initialize machine currency?
-		Currency c = Currency.getInstance(Locale.CANADA);
-		int[] noteAmounts = {5, 10, 20, 50, 100};
-		BigDecimal[] coinAmounts = {new BigDecimal("0.05"), new BigDecimal("0.10"),  new BigDecimal("0.25"), new BigDecimal("1.00"), new BigDecimal("2.00")};
-
 		Numeral[] code1Digits = {Numeral.one, Numeral.two, Numeral.three, Numeral.four};
 		Barcode barcode1 = new Barcode(code1Digits);
-
 		Numeral[] code2Digits = {Numeral.two, Numeral.three, Numeral.four, Numeral.five};
 		Barcode barcode2 = new Barcode(code2Digits);
-
 		Numeral[] code3Digits = {Numeral.six, Numeral.seven, Numeral.eight, Numeral.nine};
 		Barcode barcode3 = new Barcode(code3Digits);
 		
@@ -52,27 +68,28 @@ public class ProductsDatabase {
 		barcodes.add(barcode2);
 		barcodes.add(barcode3);
 
-			// BarcodedProduct(Barcode barcode, String description, long price, double expectedWeightInGrams)
-				// I assumed price is in cents since longs can't have decimals
 		BarcodedProduct milk = new BarcodedProduct(barcode1, "Milk", 3, 3894);
 		BarcodedProduct chocolate = new BarcodedProduct(barcode1, "Chocolate bar", 2, 49);
 		BarcodedProduct sushi = new BarcodedProduct(barcode1, "20pc Sushi", 10, 600);
 		 
-		productDatabase.put(barcode1, milk); // Adding milk to the product database with its barcode as the key
+		productDatabase.put(barcode1, milk); 
 		productDatabase.put(barcode2, chocolate);
 		productDatabase.put(barcode3, sushi);
 	}
 
+	/**
+	 * Initialize PLU codes and assign them to products 
+	 * Add the PLU-coded products to the PLU database 
+	 */
 	private static void InitializePLUProducts() {
 		PriceLookUpCode plu1 = new PriceLookUpCode("1234");
 		PriceLookUpCode plu2 = new PriceLookUpCode("2345");
 		PriceLookUpCode plu3 = new PriceLookUpCode("6789");
 		
-			//PLUCodedProduct(PriceLookUpCode pluCode, String description, long price)
-				// Assuming price in cents
 		PLUCodedProduct apple = new PLUCodedProduct(plu1, "Apple", 1);
 		PLUCodedProduct banana = new PLUCodedProduct(plu1, "Banana", 1);
 		PLUCodedProduct kiwi = new PLUCodedProduct(plu1, "Kiwi", 2);
+		
 		PLUDatabase.put(plu1, apple);
 		PLUDatabase.put(plu2, banana);
 		PLUDatabase.put(plu3, kiwi);
@@ -81,6 +98,7 @@ public class ProductsDatabase {
 	/**
 	 * Returns a list of all the barcodes available for products.
 	 * @return
+	 * 			The list of available barcodes 
 	 */
 	public static ArrayList<Barcode> getBarcodes() {
 		return barcodes;
