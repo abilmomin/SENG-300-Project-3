a935505c53a97500adc483947e272ca674b7123d
Lucas <kasdorflgm@gmail.com>
2024-04-06 18:31:55 -0600

    Remove unnecessary import

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/CardHandlerTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/CardHandlerTest.java
@@ -31,7 +31,6 @@
 
 package com.thelocalmarketplace.software.test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
